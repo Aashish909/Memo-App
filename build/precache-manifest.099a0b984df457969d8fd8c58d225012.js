b8329744d0392765daae8ffb37404b005072e5e8
Aashish909 <aashish24062000@gmail.com>
2023-08-27 13:34:44 +0530

    Memo app to create your todo list

---
+++ build/precache-manifest.099a0b984df457969d8fd8c58d225012.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7820031bd375e2af10237a656aee552e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922045d23e53f3796558",
+    "url": "/static/js/2.e25a4245.chunk.js"
+  },
+  {
+    "revision": "3c881a124f95fbfd56f2",
+    "url": "/static/js/main.2b178fb0.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
